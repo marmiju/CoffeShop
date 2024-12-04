e9dd1caa728efff3bab7c4e22c2d997c1b499352
MAR miju <mar.miju.dev@gmail.com>
2024-12-04 15:50:02 +0600

    MongoDB Connection

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000 ;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const db_username = process.env.DB_USER;
+const db_passkey = process.env.DB_PASS;
+
+//====================================
+
+
+const uri = `mongodb+srv://${db_username}:${db_passkey}@cluster0.yzj1q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 
 app.get('/',(req,res)=>{
