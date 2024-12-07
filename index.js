8e216ac9dd4ba81d6b850af317eaf7751ca38a77
MAR miju <mar.miju.dev@gmail.com>
2024-12-07 20:44:32 +0600

    get single coffee

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000 ;
 
@@ -50,6 +50,14 @@ app.get('/coffee',async(req,res)=>{
  const result =  await collection.find().toArray();
  res.send(result);
  console.log(result);
+
+})
+app.get('/coffee/:id',async(req,res)=>{
+  const id = req.params.id;
+  // console.log(id);
+  const query = {_id : new ObjectId(id)}
+  const result = await collection.findOne(query);
+  res.send(result);
 
 })
 
