0f54d78a2bc87fc5c2cac8ff5690d33ac6324ba2
MAR miju <mar.miju.dev@gmail.com>
2024-12-08 16:16:49 +0600

    delet

--- index.js
+++ index.js
@@ -60,6 +60,16 @@ app.get('/coffee/:id',async(req,res)=>{
   res.send(result);
 
 })
+app.delete('/coffee/:id', async (req, res) => {
+  const id = req.params.id;
+  
+  const query = {_id:new ObjectId(id)}
+
+  const result= await collection.deleteOne(query);
+  res.send(result);
+  
+});
+
 
 app.get('/',(req,res)=>{
     res.send('Hey! Wellcome to Cofee Bhai');
